e65dc0cdb3b5289ad11cb0883593eaae7182f5d3
Umanets Oleksii <oumanets@magisoft.solutions>
2020-12-26 12:57:04 +0200

    added map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import GlobalStyles from "./GlobalStyles.styled";
+import { Map } from "./components/Map/Map";
 
 function App() {
+  const [polygons, setPolygons] = useState([]);
+
+  const fetchData = async () => {
+    const { data } = await axios.get(
+      "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/NYC_Election_Districts_Water_Included/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=pgeojson"
+    );
+
+    setPolygons(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>kek</div>
+    <>
+      <GlobalStyles />
+      lol
+      <Map />
+    </>
   );
 }
 
